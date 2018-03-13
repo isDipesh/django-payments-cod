ab8a7aaaafca7e35b98ec687069822c836305316
Dipesh Acharya <xtranophilist@gmail.com>
2018-03-13 15:26:02 +0545

    Create CODProvider

--- django_payments_cod/__init__.py
+++ django_payments_cod/__init__.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from payments.core import BasicProvider
+from payments.forms import PaymentForm
 
 
 class CODProvider(BasicProvider):
@@ -6,4 +8,8 @@ class CODProvider(BasicProvider):
     Cash on Delivery Provider for django_payments
     """
 
-    pass
+    def get_form(self, payment, data=None):
+        return PaymentForm({}, self.get_return_url(payment), self._method, autosubmit=True)
+
+    def process_data(self, payment, request):
+        return HttpResponseRedirect(payment.get_success_url())
